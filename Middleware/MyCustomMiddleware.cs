278a55142e3894e051b41290c44a06b5be3e26b1
csjob <csjob78@gmail.com>
2025-06-15 23:24:36 +0530

    Middleware in .NET Core: The Power Behind the Pipeline 🔐

---
+++ Middleware/MyCustomMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace DotNetCoreWebAPI.Middleware
+{
+    public class MyCustomMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public MyCustomMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("Custom middleware executing.");
+            await _next(context);
+        }
+
+    }
+}
